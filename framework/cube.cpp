3dff7c275772991e3cba781a260087281737b865
Andreas-Christoph Bernstein <andreas.bernstein@uni-weimar.de>
2014-06-05 10:06:16 +0200

    Remove whitespaces.

--- framework/cube.cpp
+++ framework/cube.cpp
@@ -14,61 +14,60 @@
 
 namespace {
 
+std::array<Cube::Vertex, 36> get_cubeVertices(glm::vec3 min, glm::vec3 max) {
 
-    std::array<Cube::Vertex, 36> get_cubeVertices(glm::vec3 min, glm::vec3 max) {
+  std::array<Cube::Vertex, 36> vertex_array;
 
-        std::array<Cube::Vertex, 36> vertex_array;
+  // bottom
+  vertex_array[0] = Cube::Vertex( glm::vec3( min.x, min.y, min.z), glm::vec2( 0.0f, 0.0f));
+  vertex_array[1] = Cube::Vertex( glm::vec3( max.x, min.y, min.z), glm::vec2( 1.0f, 0.0f));
+  vertex_array[2] = Cube::Vertex( glm::vec3( min.x, min.y, max.z), glm::vec2( 0.0f, 1.0f));
+  vertex_array[3] = Cube::Vertex( glm::vec3( max.x, min.y, min.z), glm::vec2( 1.0f, 0.0f));
+  vertex_array[4] = Cube::Vertex( glm::vec3( max.x, min.y, max.z), glm::vec2( 1.0f, 1.0f));
+  vertex_array[5] = Cube::Vertex( glm::vec3( min.x, min.y, max.z), glm::vec2( 0.0f, 1.0f));
 
-		// bottom
-		vertex_array[0] = Cube::Vertex( glm::vec3( min.x, min.y, min.z), glm::vec2( 0.0f, 0.0f));
-		vertex_array[1] = Cube::Vertex( glm::vec3( max.x, min.y, min.z), glm::vec2( 1.0f, 0.0f));
-		vertex_array[2] = Cube::Vertex( glm::vec3( min.x, min.y, max.z), glm::vec2( 0.0f, 1.0f));
-		vertex_array[3] = Cube::Vertex( glm::vec3( max.x, min.y, min.z), glm::vec2( 1.0f, 0.0f));
-		vertex_array[4] = Cube::Vertex( glm::vec3( max.x, min.y, max.z), glm::vec2( 1.0f, 1.0f));
-		vertex_array[5] = Cube::Vertex( glm::vec3( min.x, min.y, max.z), glm::vec2( 0.0f, 1.0f));
+  //top
+  vertex_array[6] = Cube::Vertex( glm::vec3( min.x, max.y, min.z ), glm::vec2(0.0f, 0.0f ));
+  vertex_array[7] = Cube::Vertex( glm::vec3( min.x, max.y, max.z ), glm::vec2(0.0f, 1.0f ));
+  vertex_array[8] = Cube::Vertex( glm::vec3( max.x, max.y, min.z ), glm::vec2(1.0f, 0.0f ));
+  vertex_array[9] = Cube::Vertex( glm::vec3( max.x, max.y, min.z ), glm::vec2(1.0f, 0.0f ));
+  vertex_array[10] = Cube::Vertex( glm::vec3(min.x, max.y, max.z ), glm::vec2(0.0f, 1.0f ));
+  vertex_array[11] = Cube::Vertex( glm::vec3(max.x, max.y, max.z ), glm::vec2(1.0f, 1.0f ));
 
-		//top
-		vertex_array[6] = Cube::Vertex( glm::vec3( min.x, max.y, min.z ), glm::vec2(0.0f, 0.0f ));
-		vertex_array[7] = Cube::Vertex( glm::vec3( min.x, max.y, max.z ), glm::vec2(0.0f, 1.0f ));
-		vertex_array[8] = Cube::Vertex( glm::vec3( max.x, max.y, min.z ), glm::vec2(1.0f, 0.0f ));
-		vertex_array[9] = Cube::Vertex( glm::vec3( max.x, max.y, min.z ), glm::vec2(1.0f, 0.0f ));
-		vertex_array[10] = Cube::Vertex( glm::vec3(min.x, max.y, max.z ), glm::vec2(0.0f, 1.0f ));
-		vertex_array[11] = Cube::Vertex( glm::vec3(max.x, max.y, max.z ), glm::vec2(1.0f, 1.0f ));
-		                                             
-		//front
-		vertex_array[12] = Cube::Vertex( glm::vec3(min.x, min.y, max.z ), glm::vec2(1.0f, 0.0f ));
-		vertex_array[13] = Cube::Vertex( glm::vec3(max.x, min.y, max.z ), glm::vec2(0.0f, 0.0f ));
-		vertex_array[14] = Cube::Vertex( glm::vec3(min.x, max.y, max.z ), glm::vec2(1.0f, 1.0f ));
-		vertex_array[15] = Cube::Vertex( glm::vec3(max.x, min.y, max.z ), glm::vec2(0.0f, 0.0f ));
-		vertex_array[16] = Cube::Vertex( glm::vec3(max.x, max.y, max.z ), glm::vec2(0.0f, 1.0f ));
-		vertex_array[17] = Cube::Vertex( glm::vec3(min.x, max.y, max.z ), glm::vec2(1.0f, 1.0f ));
-                                                      
-		//back                                        
-		vertex_array[18] = Cube::Vertex( glm::vec3(min.x, min.y, min.z ), glm::vec2(0.0f, 0.0f ));
-		vertex_array[19] = Cube::Vertex( glm::vec3(min.x, max.y, min.z ), glm::vec2(0.0f, 1.0f ));
-		vertex_array[20] = Cube::Vertex( glm::vec3(max.x, min.y, min.z ), glm::vec2(1.0f, 0.0f ));
-		vertex_array[21] = Cube::Vertex( glm::vec3(max.x, min.y, min.z ), glm::vec2(1.0f, 0.0f ));
-		vertex_array[22] = Cube::Vertex( glm::vec3(min.x, max.y, min.z ), glm::vec2(0.0f, 1.0f ));
-		vertex_array[23] = Cube::Vertex( glm::vec3(max.x, max.y, min.z ), glm::vec2(1.0f, 1.0f ));
-                                                     
-		//left                                        .
-		vertex_array[24] = Cube::Vertex( glm::vec3(min.x, min.y, max.z ), glm::vec2(0.0f, 1.0f ));
-		vertex_array[25] = Cube::Vertex( glm::vec3(min.x, max.y, min.z ), glm::vec2(1.0f, 0.0f ));
-		vertex_array[26] = Cube::Vertex( glm::vec3(min.x, min.y, min.z ), glm::vec2(0.0f, 0.0f ));
-		vertex_array[27] = Cube::Vertex( glm::vec3(min.x, min.y, max.z ), glm::vec2(0.0f, 1.0f ));
-		vertex_array[28] = Cube::Vertex( glm::vec3(min.x, max.y, max.z ), glm::vec2(1.0f, 1.0f ));
-		vertex_array[29] = Cube::Vertex( glm::vec3(min.x, max.y, min.z ), glm::vec2(1.0f, 0.0f ));
-                                                      
-		//right                                       
-		vertex_array[30] = Cube::Vertex( glm::vec3(max.x, min.y, max.z ), glm::vec2(1.0f, 1.0f ));
-		vertex_array[31] = Cube::Vertex( glm::vec3(max.x, min.y, min.z ), glm::vec2(1.0f, 0.0f ));
-		vertex_array[32] = Cube::Vertex( glm::vec3(max.x, max.y, min.z ), glm::vec2(0.0f, 0.0f ));
-		vertex_array[33] = Cube::Vertex( glm::vec3(max.x, min.y, max.z ), glm::vec2(1.0f, 1.0f ));
-		vertex_array[34] = Cube::Vertex( glm::vec3(max.x, max.y, min.z ), glm::vec2(0.0f, 0.0f ));
-		vertex_array[35] = Cube::Vertex( glm::vec3(max.x, max.y, max.z ), glm::vec2(0.0f, 1.0f ));
+  //front
+  vertex_array[12] = Cube::Vertex( glm::vec3(min.x, min.y, max.z ), glm::vec2(1.0f, 0.0f ));
+  vertex_array[13] = Cube::Vertex( glm::vec3(max.x, min.y, max.z ), glm::vec2(0.0f, 0.0f ));
+  vertex_array[14] = Cube::Vertex( glm::vec3(min.x, max.y, max.z ), glm::vec2(1.0f, 1.0f ));
+  vertex_array[15] = Cube::Vertex( glm::vec3(max.x, min.y, max.z ), glm::vec2(0.0f, 0.0f ));
+  vertex_array[16] = Cube::Vertex( glm::vec3(max.x, max.y, max.z ), glm::vec2(0.0f, 1.0f ));
+  vertex_array[17] = Cube::Vertex( glm::vec3(min.x, max.y, max.z ), glm::vec2(1.0f, 1.0f ));
+
+  //back
+  vertex_array[18] = Cube::Vertex( glm::vec3(min.x, min.y, min.z ), glm::vec2(0.0f, 0.0f ));
+  vertex_array[19] = Cube::Vertex( glm::vec3(min.x, max.y, min.z ), glm::vec2(0.0f, 1.0f ));
+  vertex_array[20] = Cube::Vertex( glm::vec3(max.x, min.y, min.z ), glm::vec2(1.0f, 0.0f ));
+  vertex_array[21] = Cube::Vertex( glm::vec3(max.x, min.y, min.z ), glm::vec2(1.0f, 0.0f ));
+  vertex_array[22] = Cube::Vertex( glm::vec3(min.x, max.y, min.z ), glm::vec2(0.0f, 1.0f ));
+  vertex_array[23] = Cube::Vertex( glm::vec3(max.x, max.y, min.z ), glm::vec2(1.0f, 1.0f ));
+
+  //left                                        .
+  vertex_array[24] = Cube::Vertex( glm::vec3(min.x, min.y, max.z ), glm::vec2(0.0f, 1.0f ));
+  vertex_array[25] = Cube::Vertex( glm::vec3(min.x, max.y, min.z ), glm::vec2(1.0f, 0.0f ));
+  vertex_array[26] = Cube::Vertex( glm::vec3(min.x, min.y, min.z ), glm::vec2(0.0f, 0.0f ));
+  vertex_array[27] = Cube::Vertex( glm::vec3(min.x, min.y, max.z ), glm::vec2(0.0f, 1.0f ));
+  vertex_array[28] = Cube::Vertex( glm::vec3(min.x, max.y, max.z ), glm::vec2(1.0f, 1.0f ));
+  vertex_array[29] = Cube::Vertex( glm::vec3(min.x, max.y, min.z ), glm::vec2(1.0f, 0.0f ));
+
+  //right
+  vertex_array[30] = Cube::Vertex( glm::vec3(max.x, min.y, max.z ), glm::vec2(1.0f, 1.0f ));
+  vertex_array[31] = Cube::Vertex( glm::vec3(max.x, min.y, min.z ), glm::vec2(1.0f, 0.0f ));
+  vertex_array[32] = Cube::Vertex( glm::vec3(max.x, max.y, min.z ), glm::vec2(0.0f, 0.0f ));
+  vertex_array[33] = Cube::Vertex( glm::vec3(max.x, min.y, max.z ), glm::vec2(1.0f, 1.0f ));
+  vertex_array[34] = Cube::Vertex( glm::vec3(max.x, max.y, min.z ), glm::vec2(0.0f, 0.0f ));
+  vertex_array[35] = Cube::Vertex( glm::vec3(max.x, max.y, max.z ), glm::vec2(0.0f, 1.0f ));
 
 
-        return vertex_array;
+  return vertex_array;
         //    // bottom
         //        Cube::Vertex{ glm::vec3{ min.x, min.y, min.z }, glm::vec2{ 0.0f, 0.0f } },
         //        Cube::Vertex{ glm::vec3{ max.x, min.y, min.z }, glm::vec2{ 1.0f, 0.0f } },
@@ -84,23 +83,23 @@ namespace {
         //        Cube::Vertex{ glm::vec3{ max.x, max.y, min.z }, glm::vec2{ 1.0f, 0.0f } },
         //        Cube::Vertex{ glm::vec3{ min.x, max.y, max.z }, glm::vec2{ 0.0f, 1.0f } },
         //        Cube::Vertex{ glm::vec3{ max.x, max.y, max.z }, glm::vec2{ 1.0f, 1.0f } },
-        //                                   
-        //        // front              
+        //
+        //        // front
         //        Cube::Vertex{ glm::vec3{ min.x, min.y, max.z }, glm::vec2{ 1.0f, 0.0f } },
         //        Cube::Vertex{ glm::vec3{ max.x, min.y, max.z }, glm::vec2{ 0.0f, 0.0f } },
         //        Cube::Vertex{ glm::vec3{ min.x, max.y, max.z }, glm::vec2{ 1.0f, 1.0f } },
         //        Cube::Vertex{ glm::vec3{ max.x, min.y, max.z }, glm::vec2{ 0.0f, 0.0f } },
         //        Cube::Vertex{ glm::vec3{ max.x, max.y, max.z }, glm::vec2{ 0.0f, 1.0f } },
         //        Cube::Vertex{ glm::vec3{ min.x, max.y, max.z }, glm::vec2{ 1.0f, 1.0f } },
-        //                                    
-        //        // back                     
+        //
+        //        // back
         //        Cube::Vertex{ glm::vec3{ min.x, min.y, min.z }, glm::vec2{ 0.0f, 0.0f } },
         //        Cube::Vertex{ glm::vec3{ min.x, max.y, min.z }, glm::vec2{ 0.0f, 1.0f } },
         //        Cube::Vertex{ glm::vec3{ max.x, min.y, min.z }, glm::vec2{ 1.0f, 0.0f } },
         //        Cube::Vertex{ glm::vec3{ max.x, min.y, min.z }, glm::vec2{ 1.0f, 0.0f } },
         //        Cube::Vertex{ glm::vec3{ min.x, max.y, min.z }, glm::vec2{ 0.0f, 1.0f } },
         //        Cube::Vertex{ glm::vec3{ max.x, max.y, min.z }, glm::vec2{ 1.0f, 1.0f } },
-        //                                   
+        //
         //        // left                     .
         //        Cube::Vertex{ glm::vec3{ min.x, min.y, max.z }, glm::vec2{ 0.0f, 1.0f } },
         //        Cube::Vertex{ glm::vec3{ min.x, max.y, min.z }, glm::vec2{ 1.0f, 0.0f } },
@@ -108,8 +107,8 @@ namespace {
         //        Cube::Vertex{ glm::vec3{ min.x, min.y, max.z }, glm::vec2{ 0.0f, 1.0f } },
         //        Cube::Vertex{ glm::vec3{ min.x, max.y, max.z }, glm::vec2{ 1.0f, 1.0f } },
         //        Cube::Vertex{ glm::vec3{ min.x, max.y, min.z }, glm::vec2{ 1.0f, 0.0f } },
-        //                                    
-        //        // right                    
+        //
+        //        // right
         //        Cube::Vertex{ glm::vec3{ max.x, min.y, max.z }, glm::vec2{ 1.0f, 1.0f } },
         //        Cube::Vertex{ glm::vec3{ max.x, min.y, min.z }, glm::vec2{ 1.0f, 0.0f } },
         //        Cube::Vertex{ glm::vec3{ max.x, max.y, min.z }, glm::vec2{ 0.0f, 0.0f } },
@@ -124,7 +123,8 @@ Cube::Cube()
   : m_vao(0)
 {
 
-  std::array<Cube::Vertex, 36> cubeVertices = get_cubeVertices(glm::vec3(-1.0f), glm::vec3(1.0f));
+  std::array<Cube::Vertex, 36> cubeVertices =
+    get_cubeVertices(glm::vec3(-1.0f), glm::vec3(1.0f));
 
   glGenVertexArrays(1, &m_vao);
   glBindVertexArray(m_vao);
@@ -139,31 +139,32 @@ Cube::Cube()
   glEnableVertexAttribArray(0);
   glEnableVertexAttribArray(1);
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5*sizeof(GLfloat), nullptr);
-  glVertexAttribPointer(1, 2, GL_FLOAT, GL_TRUE, 5*sizeof(GLfloat), (GLvoid*)(3*sizeof(GLfloat)));
+  glVertexAttribPointer(1, 2, GL_FLOAT, GL_TRUE, 5*sizeof(GLfloat),
+      (GLvoid*)(3*sizeof(GLfloat)));
   glBindVertexArray(0);
 }
 
 Cube::Cube(glm::vec3 min, glm::vec3 max)
-: m_vao(0)
+  : m_vao(0)
 {
+  std::array<Cube::Vertex, 36> cubeVertices = get_cubeVertices(min, max);
 
-    std::array<Cube::Vertex, 36> cubeVertices = get_cubeVertices(min, max);
+  glGenVertexArrays(1, &m_vao);
+  glBindVertexArray(m_vao);
 
-    glGenVertexArrays(1, &m_vao);
-    glBindVertexArray(m_vao);
+  GLuint vbo;
+  glGenBuffers(1, &vbo);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
 
-    GLuint vbo;
-    glGenBuffers(1, &vbo);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(float)* 5 * cubeVertices.size()
+      , cubeVertices.data(), GL_STATIC_DRAW);
 
-    glBufferData(GL_ARRAY_BUFFER, sizeof(float)* 5 * cubeVertices.size()
-        , cubeVertices.data(), GL_STATIC_DRAW);
-
-    glEnableVertexAttribArray(0);
-    glEnableVertexAttribArray(1);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), nullptr);
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_TRUE, 5 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
-    glBindVertexArray(0);
+  glEnableVertexAttribArray(0);
+  glEnableVertexAttribArray(1);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), nullptr);
+  glVertexAttribPointer(1, 2, GL_FLOAT, GL_TRUE, 5 * sizeof(GLfloat),
+      (GLvoid*)(3 * sizeof(GLfloat)));
+  glBindVertexArray(0);
 }
 
 void Cube::draw() const
